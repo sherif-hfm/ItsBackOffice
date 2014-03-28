d5c7db24936172d366fa618bf8941c6dd109d501
sherif-hfm <sherif_hfm@yahoo.com>
2014-03-28 23:08:46 +0300

    Security Updates

--- BackOfficeBL/AppInfo.cs
+++ BackOfficeBL/AppInfo.cs
@@ -10,6 +10,10 @@ namespace BackOfficeBL
     {
         private static User mCurrentUser = null;
         public static User CurrentUser { get { return mCurrentUser; } set { mCurrentUser = value; UpdateAuditCurrentUser(); } }
+
+        private static int mCurrentYearID = 0;
+        public static int CurrentYearID { get { return mCurrentYearID; } set { mCurrentYearID = value; } }
+
         public static List<object> SystemVariables = new List<object>();
         public static string ApplicationUINamespace = "BackOfficeUI";
 
---
+++ BackOfficeBL/Security/FormFunction.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using BackOfficeDAL;
+namespace BackOfficeBL.Security
+{
+    public class FormFunction
+    {
+        public int FunctionID { get; set; }
+        public string FunctionText { get { return GetFunctionText(); } }
+        public string FunctionText_Ara { get; set; }
+        public string FunctionText_Eng { get; set; }
+
+        public static List<FormFunction> GetFormFunctions()
+        {
+            List<FormFunction> result = new List<FormFunction>();
+            NewAppsCnn newAppsCnn = new NewAppsCnn(AppSettings.CrAppSettings.NewAppsConnectionString);
+            var dbFormFunctions = from f in newAppsCnn.Sec_FormFunctions select f;
+            foreach (var dbFormFunction in dbFormFunctions)
+            {
+                FormFunction formFunction = new FormFunction();
+                formFunction.FromDbFormFunction(dbFormFunction);
+                result.Add(formFunction);
+            }
+            return result;
+        }
+
+
+        private string GetFunctionText()
+        {
+            CultureInfo cultureinfo = CultureInfo.CurrentUICulture;
+            switch (cultureinfo.Name)
+            {
+                case "en-US":
+                    return this.FunctionText_Eng;
+                case "ar-KW":
+                    return this.FunctionText_Ara;
+                default:
+                    return this.FunctionText_Eng;
+            }
+        }
+
+        public void FromDbFormFunction(Sec_FormFunctions _dbFormFunction)
+        {
+            this.FunctionID = _dbFormFunction.FunctionID;
+            this.FunctionText_Ara = _dbFormFunction.FunctionText_Ara;
+            this.FunctionText_Eng = _dbFormFunction.FunctionText_Eng;
+        }
+    }
+}
